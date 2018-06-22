a9c29626488dabf5c38821c2c2658cc7efff2c0b
Benj3885 <34089380+Benj3885@users.noreply.github.com>
2018-06-22 23:38:19 +0200

    Update tempLog.cpp

--- tempLog.cpp
+++ tempLog.cpp
@@ -11,7 +11,7 @@ tempLog::tempLog()
    int temppastdays[5] = {23,19,16,26,14}; // temperature for last 1-5 days ago
 
    cout << temppastdays[0] << " Celcius" << endl; // shows the temperature 1 days ago
-   cout << temppastdays[1] << " Celsius" << endl ; // shows the temperature 2 days ago
+   cout << temppastdays[1] << " Celsius" << endl; // shows the temperature 2 days ago
    cout << temppastdays[2] << " Celsius" << endl; // shows the temperature 3 days ago
    cout << temppastdays[3] << " Celcius" << endl; // shows the temperature 4 days ago
    cout << temppastdays[4] << " Celcius" << endl; // shows the temperature 5 days ago
