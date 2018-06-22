516984eca221ca9b6ea35017264bfda03e8ef489
Benj3885 <34089380+Benj3885@users.noreply.github.com>
2018-06-22 13:10:04 +0200

    Update temphu5.ino

--- temphu5.ino
+++ temphu5.ino
@@ -1,26 +1,22 @@
-#include <DHT.h>
+#include <DHT.h> // Library for the functions needed to interact with the sensor DHT22 (temperature and humidity)
 
-DHT dht;
+DHT dht; // DHT object
 
 void setup()
 {
-Serial.begin(9600);
-dht.setup(2); // data pin 2
-
+  Serial.begin(9600); // baud rate, which is bits per second
+  dht.setup(2); // The sensor pin
 }
 
 void loop() {
+  // prints out the value of the humidity and temperature in the serial monitor
+  delay(dht.getMinimumSamplingPeriod()); // 
+  Serial.print("Humidity: ");
+  Serial.print(dht.getHumidity());
+  Serial.print("\t");
+  Serial.print(" %, Temp: ");
+  Serial.print(dht.getTemperature());
+  Serial.println(" Celcius"); 
 
-
-// prints out the value of the humidity and temperature in the serial monitor
-  
-delay(dht.getMinimumSamplingPeriod());
-Serial.print("Humidity: ");
-Serial.print(dht.getHumidity());
-Serial.print("\t");
-Serial.print(" %, Temp: ");
-Serial.print(dht.getTemperature());
-Serial.println(" Celcius"); 
-
-delay(1000); //Delay with 1 second
+  delay(1000); //Delay with 1 second
 }
